3a0494958018b8d7bc349df8778a682cca8e8a15
Gizer <gabriellartey16@gmail.com>
2022-11-23 12:37:04 +0000

    functions1.c updated

--- functions1.c
+++ functions1.c
@@ -106,7 +106,7 @@ int width, int precision, int size)
 int print_hexa_upper(va_list types, char buffer[], int flags, int width,
 	int precision, int size)
 {
-	return (print_hexa(types, "0123456789ABCDEF", buffer, flags, 'x',
+	return (print_hexa(types, "0123456789ABCDEF", buffer, flags, 'X',
 	width, precision, size));
 }
 
